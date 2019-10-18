b9a4c3a733785ef62b3420defe9da9dcaf8b7212
ffowerop <sctp2k@gmail.com>
2019-10-19 00:56:19 +0300

    Add HomeWork3

---
+++ HomeWork3/script.js
@@ -0,0 +1,22 @@
+var money = prompt("Ваш бюджет на месяц?", "100000");
+var time = prompt("Введите дату в формате YYYY-MM-DD", "2011-08-07");
+var appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+var mandatoryExpenseItem = prompt("Введите обязательную статью расходов в этом месяце", "Advertising");
+var mandatoryItemPrice = prompt("Во сколько обойдется?", "18000");
+appData.expenses[mandatoryExpenseItem] = mandatoryItemPrice;
+
+mandatoryExpenseItem = prompt("Введите обязательную статью расходов в этом месяце", "Advertising");
+mandatoryItemPrice = prompt("Во сколько обойдется?", "18000");
+appData.expenses[mandatoryExpenseItem] = mandatoryItemPrice;
+
+alert("Ваш бюджет на один день составляет " + Number(appData.expenses[mandatoryExpenseItem]) / 30);
+
+console.log(appData);
