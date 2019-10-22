0529190d71b5ec20153bceacbfc8d76194dbabb7
ffowerop <sctp2k@gmail.com>
2019-10-23 01:53:41 +0300

    HW8 added

---
+++ HomeWork8/js/main.js
@@ -0,0 +1,30 @@
+let btnStart = document.querySelector('#start');
+let rightBlocks = document.querySelector('.result-table').children;
+let inputExpensesItem = document.querySelectorAll('input.expenses-item');
+let btnOptionalExpenses = document.getElementsByTagName('button')[1];
+let btnCountBudget = document.getElementsByTagName('button')[2];
+let inputOptionalExpensesItem = document.querySelectorAll('input.optionalexpenses-item');
+
+let inputChooseIncome = document.querySelector('input.choose-income');
+let inputSavings = document.querySelector('input#savings');
+let inputSum = document.querySelector('input#sum');
+let inputPercent = document.querySelector('input#percent');
+let inputYear = document.querySelector('input.year-value');
+let inputMonth = document.querySelector('input.month-value');
+let inputDay = document.querySelector('input.day-value');
+
+
+console.log(rightBlocks);
+console.log(inputExpensesItem);
+console.log(inputExpensesItem);
+console.log(btnOptionalExpenses);
+console.log(btnCountBudget);
+console.log(inputOptionalExpensesItem);
+
+console.log(inputChooseIncome);
+console.log(inputSavings);
+console.log(inputSum);
+console.log(inputPercent);
+console.log(inputYear);
+console.log(inputMonth);
+console.log(inputDay);
