47b4fe627db764b6b2c7203e607c072b6cf4435e
ffowerop <sctp2k@gmail.com>
2019-10-25 05:30:52 +0300

    hw10 done

--- HomeWork10/js/script.js
+++ HomeWork10/js/script.js
@@ -41,4 +41,39 @@ window.addEventListener('DOMContentLoaded', function() {
             showTabContent(contentTab[menuTab.indexOf(event.target)]);
         }
     });
+
+    /// timer \\\
+    
+    const DEADLINE = '2019-10-27';
+
+    function getTimeInterval(deadline) {
+        let intervalSeconds = Math.floor((new Date(deadline) - Date.now()) / 1000);
+
+        return {
+            total : intervalSeconds,
+            hours : Math.floor(intervalSeconds/3600),
+            minutes : Math.floor(intervalSeconds/60)%60,
+            seconds : intervalSeconds%60,
+        };
+    }
+
+    (function setClock(id, endtime) {
+        const timerBlock = document.getElementById(id);
+        const hours = timerBlock.querySelector('.hours');
+        const minutes = timerBlock.querySelector('.minutes');
+        const seconds = timerBlock.querySelector('.seconds');
+
+        (function updateClock() {
+            const interval = getTimeInterval(DEADLINE);
+            if (interval.total <= 0) {
+                timerBlock.innerHTML = '00:00:00';
+                return;
+            }
+            hours.textContent = (interval.hours).toString().padStart(2,'0');
+            minutes.textContent = (interval.minutes).toString().padStart(2,'0');
+            seconds.textContent = (interval.seconds).toString().padStart(2,'0');
+            
+            setTimeout(updateClock, 1000);
+        })();
+    })('timer');
 });
